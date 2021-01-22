9d53a093f3fcd895f677fe5283ac784ac25c2780
root <root@ip-172-31-19-178.us-east-2.compute.internal>
2021-01-22 09:48:45 +0000

    intial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World this is my new app github trigger function!'))
+app.get('/', (req, res) => res.send('Hello World this is my new app github trigger function everything working fine!'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
